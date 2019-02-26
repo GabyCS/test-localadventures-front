6634c95d619039e90f3603e8825e2fd39631288c
Gaby Casillas <csgaby7@gmail.com>
2019-02-26 12:07:06 -0600

    header was added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './containers/Dashboard';
+import Header from './components/Header';
 import ComponentTestRedux from './components/testComponents';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <BrowserRouter>
           <Switch>
             <Route exact path='/' component={Dashboard} />
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+                <h1>HEADER</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
