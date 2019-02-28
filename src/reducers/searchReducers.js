5957488afece41d4e568c22ec6c2913a5635b16b
Gaby Casillas <csgaby7@gmail.com>
2019-02-28 01:01:59 -0600

    reducer was added

--- src/reducers/searchReducers.js
+++ src/reducers/searchReducers.js
@@ -1,9 +1,17 @@
 const searchRedurcer = (state = {
     showFormSearch: true,
     repositories:false,
+    repository: false,
+    commits: false,
     getRepositoriesFetch:false,
     getRepositoriesSuccess:false,
-    getRepositoriesFailure:false
+    getRepositoriesFailure:false,
+    getRepositoryFetch:false,
+    getRepositorySuccess:false,
+    getRepositoryFailure:false,
+    getCommitsFetch:false,
+    getCommitsSuccess:false,
+    getCommitsFailure:false
 }, action) => {
     switch(action.type){
         case "SET_STATE_FORM_SEARCH":
@@ -26,12 +34,56 @@ const searchRedurcer = (state = {
                 getRepositoriesSuccess:true,
                 getRepositoriesFailure:false
             }
-        case "GET_TAREAS_FAILURE":
+        case "GET_REPOSITORIES_FAILURE":
             return {
                 repositories:false,
                 getRepositoriesFetch:false,
                 getRepositoriesSuccess:false,
                 getRepositoriesFailure:true
+            }
+        case "GET_REPOSITORY_FETCHING":
+            return {
+                ...state,
+                repository: false,
+                getRepositoryFetch:true,
+                getRepositorySuccess:false,
+                getRepositoryFailure:false
+            }
+        case "GET_REPOSITORY_SUCESS":
+            return {
+                repository: action.data,
+                getRepositoryFetch:false,
+                getRepositorySuccess:true,
+                getRepositoryFailure:false
+            }
+        case "GET_REPOSITORY_FAILURE":
+            return {
+                repository:false,
+                getRepositoryFetch:false,
+                getRepositorySuccess:false,
+                getRepositoryFailure:true
+            }
+            case "GET_COMMITS_FETCHING":
+            return {
+                ...state,
+                commits: false,
+                getCommitsFetch:true,
+                getCommitsSuccess:false,
+                getCommitsFailure:false
+            }
+        case "GET_COMMITS_SUCESS":
+            return {
+                commits: action.data,
+                getCommitsFetch:false,
+                getCommitsSuccess:true,
+                getCommitsFailure:false
+            }
+        case "GET_COMMITS_FAILURE":
+            return {
+                commits:false,
+                getCommitsFetch:false,
+                getCommitsSuccess:false,
+                getCommitsFailure:true
             }
         default:
             break;
