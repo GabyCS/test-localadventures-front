aa6f613e854dee36b090f2e612a5e7b1480e11a7
Gaby Casillas <csgaby7@gmail.com>
2019-02-28 04:38:07 -0600

    cambio url heroku

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const dev = false;
-const URL_APP = (dev) ? 'http://localhost:8000':'https://app-test-git.herokuapp.com';
+const URL_APP = (dev) ? 'http://localhost:8000':'https://api-test-git.herokuapp.com';
 
 module.exports = { URL_APP };
